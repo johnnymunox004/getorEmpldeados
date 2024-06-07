615b2bd82f5753cd338cfc48bdbdf84dd18535aa
josejose <johnnymu0050@gmail.com>
2024-06-06 23:46:48 -0500

    Mensaje descriptivo de los cambios realizados

--- Frontend/src/components/navLinks.jsx
+++ Frontend/src/components/navLinks.jsx
@@ -7,42 +7,42 @@ import React from "react";
 
 const NavLinks = () => {
   return (
-    <div className="flex flex-col items-center bg-gray-800 p-4 rounded-lg shadow-md space-y-4">
+    <div className="flex flex-col items-center bg-gray-800 p-4 rounded-lg shadow-md space-y-4 gap-8">
       <Link
         to={"/"}
         className="text-white hover:text-yellow-300 transition-colors duration-300"
       >
+        salir
         <IoLogIn className="text-2xl" />
       </Link>
+
       <Link
         to={"/dashboard/list-empleados"}
         className="text-white hover:text-yellow-300 transition-colors duration-300"
-      >
+      > empleados
         <FaHome className="text-2xl" />
       </Link>
+
       <Link
         to={"/dashboard/list-aspirant"}
         className="text-white hover:text-yellow-300 transition-colors duration-300"
-      >
+      >aspirntes
         <BsFillPersonLinesFill className="text-2xl" />
       </Link>
 
       <Link
         to={"/notificaciones"}
         className="text-white hover:text-yellow-300 transition-colors duration-300"
-      >
+      >notificaciones
         <FaBell className="text-2xl" />
       </Link>
 
       <Link
         to={"/creaaenotificaciones"}
         className="text-white hover:text-yellow-300 transition-colors duration-300"
-      >
+      > crear notificaacionn
         <FaBell className="text-2xl" />
       </Link>
-
-     
-      
     </div>
   );
 };
